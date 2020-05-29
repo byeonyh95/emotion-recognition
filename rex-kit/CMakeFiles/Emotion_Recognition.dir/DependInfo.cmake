f0654e2e106fd512085513654c667ca3b7257c54
byeonyh95 <byeonyh95@gmail.com>
2020-05-29 15:44:46 +0900

    first commit

---
+++ rex-kit/CMakeFiles/Emotion_Recognition.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/byeonyh/emotion2/src/buffer.c" "/home/byeonyh/emotion2/CMakeFiles/Emotion_Recognition.dir/src/buffer.c.o"
+  "/home/byeonyh/emotion2/src/yuv.c" "/home/byeonyh/emotion2/CMakeFiles/Emotion_Recognition.dir/src/yuv.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "common/include/libdrm"
+  "common/include/libdrm/libdrm"
+  "common/include"
+  "common/include/libv4l2"
+  "common/include/rga"
+  "inc"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/byeonyh/emotion2/common/src/camera_helper.cc" "/home/byeonyh/emotion2/CMakeFiles/Emotion_Recognition.dir/common/src/camera_helper.cc.o"
+  "/home/byeonyh/emotion2/common/src/drm_module.cc" "/home/byeonyh/emotion2/CMakeFiles/Emotion_Recognition.dir/common/src/drm_module.cc.o"
+  "/home/byeonyh/emotion2/common/src/rga_helper.cc" "/home/byeonyh/emotion2/CMakeFiles/Emotion_Recognition.dir/common/src/rga_helper.cc.o"
+  "/home/byeonyh/emotion2/common/src/rknn_helper.cc" "/home/byeonyh/emotion2/CMakeFiles/Emotion_Recognition.dir/common/src/rknn_helper.cc.o"
+  "/home/byeonyh/emotion2/src/main.cc" "/home/byeonyh/emotion2/CMakeFiles/Emotion_Recognition.dir/src/main.cc.o"
+  "/home/byeonyh/emotion2/src/objects_tracker.cpp" "/home/byeonyh/emotion2/CMakeFiles/Emotion_Recognition.dir/src/objects_tracker.cpp.o"
+  "/home/byeonyh/emotion2/src/objects_update.cpp" "/home/byeonyh/emotion2/CMakeFiles/Emotion_Recognition.dir/src/objects_update.cpp.o"
+  "/home/byeonyh/emotion2/src/ssd.cc" "/home/byeonyh/emotion2/CMakeFiles/Emotion_Recognition.dir/src/ssd.cc.o"
+  "/home/byeonyh/emotion2/src/track_c_link_c++.cpp" "/home/byeonyh/emotion2/CMakeFiles/Emotion_Recognition.dir/src/track_c_link_c++.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "common/include/libdrm"
+  "common/include/libdrm/libdrm"
+  "common/include"
+  "common/include/libv4l2"
+  "common/include/rga"
+  "inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ rex-kit/CMakeFiles/Emotion_Recognition.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Emotion_Recognition.dir/common/src/camera_helper.cc.o"
+  "CMakeFiles/Emotion_Recognition.dir/common/src/drm_module.cc.o"
+  "CMakeFiles/Emotion_Recognition.dir/common/src/rga_helper.cc.o"
+  "CMakeFiles/Emotion_Recognition.dir/common/src/rknn_helper.cc.o"
+  "CMakeFiles/Emotion_Recognition.dir/src/buffer.c.o"
+  "CMakeFiles/Emotion_Recognition.dir/src/yuv.c.o"
+  "CMakeFiles/Emotion_Recognition.dir/src/main.cc.o"
+  "CMakeFiles/Emotion_Recognition.dir/src/ssd.cc.o"
+  "CMakeFiles/Emotion_Recognition.dir/src/objects_tracker.cpp.o"
+  "CMakeFiles/Emotion_Recognition.dir/src/objects_update.cpp.o"
+  "CMakeFiles/Emotion_Recognition.dir/src/track_c_link_c++.cpp.o"
+  "Emotion_Recognition.pdb"
+  "Emotion_Recognition"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Emotion_Recognition.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
